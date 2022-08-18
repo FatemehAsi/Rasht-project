18b1ab9798de95a01b1b4f09081b757f10edb84b
Fatemeh <fatemeh.asi.2000@gmail.com>
2022-08-18 20:18:04 +0430

    added js file of project

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("b1").addEventListener("click", show1);
+function show1() {
+  document.getElementById("h1-text").innerHTML = "Rasht has many natural attractions to visit, many forest parks, and amazing northern foods.";
+}
+
+document.getElementById("b2").addEventListener("click", show2);
+function show2() {
+  document.getElementById("h2-text").innerHTML = "Rasht is best known for its weather, food, Shahrdari Square, and Saravan Forest Park. It is also known as the city of silver rains.";
+}
+
+document.getElementById("b3").addEventListener("click", show3);
+function show3() {
+  document.getElementById("h3-text").innerHTML = "Registered as Creative City of gastronomy in UNESCO, Rasht offers a huge range of delicious foods. The most well-known foods in Rasht are: Mirza Qasemi, Torsh-e Tareh, Anar-Bij, Sir-Qelyeh, and Baqali-Qatoq.";
+}
+
+document.getElementById("b4").addEventListener("click", show4);
+function show4() {
+  document.getElementById("h4-text").innerHTML = "Rasht has many beautiful things to offer. But make sure to checkout Shahrdari Square, Gilan rural heritage museum, Saravan Lake, Rasht Grand Bazaar, and Saqalaksar Lake.";
+}
+
+document.getElementById("b5").addEventListener("click", show5);
+function show5() {
+  document.getElementById("h5-text").innerHTML = "The population of Rasht is 679,995 people(2016).";
+}
+
+document.getElementById("b6").addEventListener("click", show6);
+function show6() {
+  document.getElementById("h6-text").innerHTML = "You can take a taxi and enjoy the beautiful city of Rasht easily.";
+}
+
+document.getElementById("b7").addEventListener("click", show7);
+function show7() {
+  document.getElementById("h7-text").innerHTML = "Rasht is a lively city you can enjoy every time of the day.";
+}
